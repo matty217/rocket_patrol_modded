7c381b78c0f0d47ba12414b38bf29d24c5e87563
matty217 <mathill@ucsc.edu>
2022-04-20 16:05:44 -0700

    grading message

--- src/main.js
+++ src/main.js
@@ -1,3 +1,26 @@
+// Matthew Hill
+// Feeding Time!
+// 20 Hours
+
+//60 pts - Asset/Theme Overhaul
+//30 pts - Simultaneous 2 Player
+//10 pts - Display Time on Screen
+
+//MISC
+//randomize enemy speed and direction on reset
+//new player character that fires the 'rockets'
+//Two Player mode and single player mode both available
+
+
+
+
+
+
+
+
+
+
+
 let config = {
     type: Phaser.CANVAS,
     width: 840, //640,
